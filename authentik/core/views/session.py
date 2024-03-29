b90d12c3ccca05851f2e9ff5651f92807ffd8592
Armando Capozza <armando.capozza@zupit.it>
2024-03-29 16:09:10 +0100

    fix: first fixes

--- authentik/core/views/session.py
+++ authentik/core/views/session.py
@@ -2,22 +2,19 @@
 
 from typing import Any
 
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.base import TemplateView
 
 from authentik.core.models import Application
+from authentik.lib.utils.urls import redirect_with_qs
 from authentik.policies.views import PolicyAccessView
 
 
 class EndSessionView(TemplateView, PolicyAccessView):
     """Allow the client to end the Session"""
 
-    template_name = "if/end_session.html"
-
     def resolve_provider_application(self):
         self.application = get_object_or_404(Application, slug=self.kwargs["application_slug"])
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context["application"] = self.application
-        return context
+    def get(self, request, *args, **kwargs):
+        return redirect('authentik_flows:default-invalidation')
